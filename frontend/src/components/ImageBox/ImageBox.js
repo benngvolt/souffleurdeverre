a15ac2702f61a4f0660f1a7a91f977949a03e8e7
Benjamin Gibert <contact@benjamingibert.com>
2025-04-29 08:59:33 +0200

    ajustements

--- frontend/src/components/ImageBox/ImageBox.js
+++ frontend/src/components/ImageBox/ImageBox.js
@@ -1,12 +1,18 @@
 import './ImageBox.scss'
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faXmark} from '@fortawesome/free-solid-svg-icons'
+
  
 function ImageBox({imageUrl, setHandleDisplayImage}) {
 
 
     return  (      
         <div className='imageBox' >
-            <img src={imageUrl} onClick={(e) => e.stopPropagation()}/>
+            <div class="imageBox_imageWrapper">
+                <img src={imageUrl} onClick={(e) => e.stopPropagation()}/>
+                <FontAwesomeIcon icon={faXmark}/>
+            </div>
         </div>
     )
 }
